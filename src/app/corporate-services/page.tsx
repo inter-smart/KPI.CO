2aaabf812161aa25c393971c1a56b72478f4a674
abish <abish@intersmart.in>
2026-02-05 14:14:06 +0530

    name changes

--- src/app/corporate-services/page.tsx
+++ src/app/corporate-services/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
 export default function CorporateServicesPage() {
     return (
-        <div>Abish do the full page and impress everyone</div>
+        <div>Nothing Matters</div>
     )
 }
