cce6c9407e7c62d9e117e441916d8937ecc51048
amrutha <amrutha@intersmart.in>
2026-02-18 19:00:29 +0530

    corrections

--- src/components/features/services/CorporateServicesUaeWhyBuild.tsx
+++ src/components/features/services/CorporateServicesUaeWhyBuild.tsx
@@ -48,8 +48,13 @@ function WhyBuildCard({
   variant?: Variant | Variant[];
 }) {
   return (
-    <div className="group w-full h-full xl:min-h-[230px] 2xl:min-h-[276px] 3xl:min-h-[340px] bg-white rounded-[10px] p-5 sm:p-4 xl:py-6 2xl:py-7.5 xl:px-5 2xl:px-5.5 shadow-[0px_0px_5px_0_rgba(28,83,150,0.1)] hover:shadow-[0px_10px_30px_rgba(28,83,150,0.1)] transition-all duration-300">
-      <div className="w-[46px] 2xl:w-[55px] aspect-square mb-[20px] 2xl:mb-[25px] transition-transform  ">
+    <div
+      className={cn(
+        "group w-full h-full xl:min-h-[230px] 2xl:min-h-[276px] 3xl:min-h-[340px] bg-white rounded-[10px] p-5 sm:p-4 xl:py-6 2xl:py-7.5 xl:px-5 2xl:px-5.5 shadow-[0px_0px_5px_0_rgba(28,83,150,0.1)] hover:shadow-[0px_10px_30px_rgba(28,83,150,0.1)] transition-all duration-300",
+        variant === "freezone" ? "xl:min-h-[205px] 2xl:min-h-[265px] 3xl:min-h-[300px]" : "",
+      )}
+    >
+      <div className="w-[46px] 2xl:w-[55px] aspect-square mb-[20px] 2xl:mb-[25px] transition-transform">
         <Image
           src={item.media.path}
           alt={item.media.alt}
@@ -64,7 +69,7 @@ function WhyBuildCard({
         className={cn(
           "font-semibold !text-black mb-2 2xl:mb-2.5",
           (hasVariant(variant, "saifz") || hasVariant(variant, "freezone")) &&
-          "text-[#1C5396]",
+            "text-[#1C5396]",
           "font-semibold text-[#1C5396] mb-2 2xl:mb-2.5",
           hasVariant(variant, "saifz") && "text-[#1C5396]",
           hasVariant(variant, "dafz") && "text-[#1C5396]",
@@ -107,7 +112,7 @@ export default function CorporateServicesUaeWhyBuild({
           ? "bg-[#f9fafb] "
           : "bg-linear-to-t from-[#f1fafe] via-white to-white",
         hasVariant(variant, "saifz") &&
-        "bg-linear-to-t from-[#f1fafe] via-white to-white",
+          "bg-linear-to-t from-[#f1fafe] via-white to-white",
       )}
     >
       <div className="container">
@@ -144,7 +149,7 @@ export default function CorporateServicesUaeWhyBuild({
               ? "lg:grid-cols-4 gap-3 xl:gap-5 2xl:gap-6"
               : "lg:grid-cols-3 gap-4 xl:gap-8 2xl:gap-10",
             hasVariant(variant, "dafz") &&
-            "lg:!gap-[20px_30px] xl:!gap-[25px_35px] 2xl:!gap-[25px_40px] 3xl:!gap-[35px_50px]",
+              "lg:!gap-[20px_30px] xl:!gap-[25px_35px] 2xl:!gap-[25px_40px] 3xl:!gap-[35px_50px]",
           )}
         >
           {data.items.map((item) => (
